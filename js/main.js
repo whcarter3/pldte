aaee6501ea7717512739951c823cf993fdd1acc0
Will Carter <whcarter3@gmail.com>
2016-02-07 01:31:08 -0800

    added nave and scroll to fixed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	//Main Logo Animation
 	var $mainLogo = $('#main-logo');
 	var $numLogos = $('#main-logo img').filter(function(){
 		return this.id.match(/pd-logo-\d/)
@@ -13,33 +15,53 @@ $(document).ready(function(){
 		return this.id.match(/pd-logo-4/)
 	});
 
-	var logoAnimate = TweenMax.from($mainLogo, 1, {
-		left: '105%',
+	var logoAnimate = TweenMax.to($mainLogo, 1, {
+		left: 0,
 		ease: Back.easeInOut,
 		delay: 2
 	});
 
-	var dateAnimate1 = TweenMax.to($numLogos, .5, {
-		top: '+=200px',
+	var dateAnimate1 = TweenMax.to($numLogos, .3, {
+		top: '+=15vh',
 		ease: Power0.easeNone,
 		delay: 3
 	});
 
-	var dateAnimate2 = TweenMax.to($numLogos2up, .5, {
-		top: '+=200px',
+	var dateAnimate2 = TweenMax.to($numLogos2up, .3, {
+		top: '+=15vh',
 		ease: Power0.easeNone,
-		delay: 3.5
+		delay: 3.3
 	});
 
-	var dateAnimate3 = TweenMax.to($numLogos3up, .5, {
-		top: '+=200px',
+	var dateAnimate3 = TweenMax.to($numLogos3up, .3, {
+		top: '+=15vh',
 		ease: Power0.easeNone,
-		delay: 4
+		delay: 3.6
 	});
 
-	var dateAnimate4 = TweenMax.to($numLogos4up, .5, {
-		top: '+=200px',
+	var dateAnimate4 = TweenMax.to($numLogos4up, .3, {
+		top: '+=15vh',
 		ease: Power0.easeNone,
-		delay: 4.5
+		delay: 3.9
 	});
-})
+
+	//Scroll to fixed nav
+
+	//position of navbar
+	var $navbar 	= $('.navbar-default');
+	var navOffset 	= $navbar.offset().top;
+
+	$navbar.wrap('<div class="nav-placeholder"></div>')
+	$('.nav-placeholder').height($navbar.outerHeight());
+
+	//calculate scroll position on page
+	$(window).scroll(function(){
+		var scrollPos = $(window).scrollTop();
+
+		if(scrollPos >= navOffset){
+			$navbar.addClass('fixed');
+		} else {
+			$navbar.removeClass('fixed');
+		}
+	});
+});
